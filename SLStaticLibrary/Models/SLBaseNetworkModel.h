6cf783d84f4729e7eec57707a1f677c62bf20aab
mc.sugar <339426723@qq.com>
2015-03-04 11:35:30 +0800

    修改说明。

--- SLStaticLibrary/Models/SLBaseNetworkModel.h
+++ SLStaticLibrary/Models/SLBaseNetworkModel.h
@@ -8,10 +8,11 @@
 
 @interface SLBaseNetworkModel : NSObject
 
-@property (nonatomic, strong, readonly) id jsonModel;
+@property (nonatomic, strong, readonly) id jsonModel;   // 原 Json 数据
 
 - (instancetype)initWithJsonModel:(id)jsonModel;
 
+// 设置属性配置，SLPropertyCorrespondenceModel 类型，需在 initWithJsonModel: 前调用
 - (void)setPropertyCorrespondences:(NSDictionary *)newPropertyCorrespondences;
 
 @end
