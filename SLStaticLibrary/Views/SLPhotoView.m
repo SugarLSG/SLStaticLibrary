71c61763eb53b37436b5f17e3583c39c3e0520a3
mc.sugar <339426723@qq.com>
2015-03-12 15:42:48 +0800

    fix.

--- SLStaticLibrary/Views/SLPhotoView.m
+++ SLStaticLibrary/Views/SLPhotoView.m
@@ -75,7 +75,7 @@
     self.btnPrev.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5f];
     self.btnPrev.layer.cornerRadius = self.btnPrev.frame.size.width / 2;
     self.btnPrev.hidden = YES;
-    [self.btnPrev setImage:[UIImage imageNamed:[@"Images.bundle" stringByAppendingPathComponent:@"arrow.left.png"]] forState:UIControlStateNormal];
+    [self.btnPrev setImage:[UIImage imageNamed:[@"SLImages.bundle" stringByAppendingPathComponent:@"arrow.left.png"]] forState:UIControlStateNormal];
     self.btnPrev.imageEdgeInsets = UIEdgeInsetsMake(12, 15, 12, 15);
     [self.btnPrev addTarget:self action:@selector(btnPrevClick:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:self.btnPrev];
@@ -86,7 +86,7 @@
     self.btnNext.backgroundColor = self.btnPrev.backgroundColor;
     self.btnNext.layer.cornerRadius = self.btnNext.frame.size.width / 2;
     self.btnNext.hidden = YES;
-    [self.btnNext setImage:[UIImage imageNamed:[@"Images.bundle" stringByAppendingPathComponent:@"arrow.left.png"]] forState:UIControlStateNormal];
+    [self.btnNext setImage:[UIImage imageNamed:[@"SLImages.bundle" stringByAppendingPathComponent:@"arrow.left.png"]] forState:UIControlStateNormal];
     self.btnNext.imageEdgeInsets = UIEdgeInsetsMake(12, 15, 12, 15);
     self.btnNext.transform = CGAffineTransformMakeRotation(M_PI);
     [self.btnNext addTarget:self action:@selector(btnNextClick:) forControlEvents:UIControlEventTouchUpInside];
