222b5727bb77ecb7e90038f1b4678c9ffd14cbcd
mc.sugar <339426723@qq.com>
2015-03-25 10:29:46 +0800

    增加用户设置（本地存储）.

--- SLStaticLibrary/Commons/SLStaticLibrary.h
+++ SLStaticLibrary/Commons/SLStaticLibrary.h
@@ -38,3 +38,6 @@
 /* app color */
 #define generateColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
 #define generateColorWithHexValue(v, a) [UIColor colorWithHexValue:(v) alpha:(a)]
+
+/* user setting */
+#define userSettings [NSUserDefaults standardUserDefaults]
